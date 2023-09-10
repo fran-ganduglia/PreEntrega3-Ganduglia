83ca1e9590f1a46618f88e9abc88a420fe9f7d1f
Francisco Ganduglia <franciscoganduglia@gmail.com>
2023-09-10 13:54:45 -0300

    Trabajo terminado

--- js/carrito.js
+++ js/carrito.js
@@ -1,78 +1,28 @@
 const shopContent = document.getElementById("shopContent")
 const verCarrito = document.getElementById("verCarrito")
 const modalContainer = document.getElementById("modalContainer")
-const productos = [
-    { id: 1,
-         
-        nombre: "G-435", 
-        precio: 49.99, 
-        stock: 20,
-        img: "../images/auriculares 1.png"
-    },
+const compraFinal = document.getElementById("contCompra")
 
-    { id: 2,
-         
-        nombre: "G-733", 
-        precio: 67, 
-        stock: 16,
-        img: "../images/auriculares 2.png"
-    },
+let carrito = JSON.parse (localStorage.getItem("data")) || [];
 
-    { id: 3,
-         
-        nombre: "G-335 Pro",
-        precio: 50, 
-        stock: 18, 
-        img: "../images/auriculares 3.png"
-    },
+const saveLocal = ()  =>  {
+  localStorage.setItem("data", JSON.stringify(carrito));
+}
 
-    { id: 4,
-         
-        nombre: "G-332", 
-        precio: 25.99, 
-        stock: 5, 
-        img: "../images/auriculares 4.png"
-    },
+const carritoCounter = () => {
+  cantidadCarrito.style.display = "block";
 
-    { id: 5,
-         
-        nombre: "Signature K-650", 
-        precio: 80, 
-        stock: 8, 
-        img: "../images/teclado 1.png"
-    },
+  const carritoLength = carrito.length;
 
-    { id: 6,
-         
-        nombre: "MX Mechanical", 
-        precio: 85.99, 
-        stock: 13, 
-        img: "../images/teclado 2.png"
-    },
+  localStorage.setItem("carritoLength", JSON.stringify(carritoLength));
 
-    { id: 7,
-         
-        nombre: "ERGO K-860", 
-        precio: 100, 
-        stock: 6, 
-        img: "../images/teclado 3.png"
-    },
+  cantidadCarrito.innerText = JSON.parse(localStorage.getItem("carritoLength"));
+};
 
-    { id: 8,
-         
-        nombre: "Teclado K-835", 
-        precio: 82, 
-        stock: 18, 
-        img: "../images/teclado 4.png"
-    },
-
-    ];
-
-JSON.parse(localStorage.getItem("productos")) || localStorage.setItem("productos", JSON.stringify(productos))
-
-let carrito = [];
-
-productos.forEach((product) => {
+fetch("../data.json")
+.then(response => response.json())
+.then(data => {
+  data.forEach((product) => {
     let content = document.createElement("div");
     content.innerHTML = `
     <img src="${product.img}" class="card-img-top  card-img-mobile" alt="article-img-3">
@@ -85,116 +35,218 @@ productos.forEach((product) => {
 
     shopContent.append(content)
 
-
     let comprar = document.getElementById (`btn${product.id}`)
     
+
     comprar.addEventListener("click", () =>{
 
-        carrito.push({
+
+      Swal.fire({
+      title:'Su producto se ha agregado correctamente!',
+      showCloseButton: true,
+      icon: 'success',
+      })
+
+
+
+        const repeat = data.some((repeatProduct) => repeatProduct.id === product.id);
+
+        if (repeat) {
+          carrito.map((prod) => {
+            if (prod.id === product.id) {
+              prod.cant++;
+            }
+          });
+        
+          carrito.push({
             id: product.id,
             img: product.img,
             nombre: product.nombre,
             precio: product.precio,
-        })
-    })
+            cant: product.cant,
+          });
+        carritoCounter();
+        saveLocal();
+        
+        
+    }})
+})
 })
 
-
-verCarrito.addEventListener("click", () => {
-  modalContainer.innerHTML= " ";
-  modalContainer.style.display = "flex";
-  const modalHeader = document.createElement("div");
-  modalHeader.className = "modal-header";
-  modalHeader.innerHTML = `
+const pintarCarrito = () => {
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML = `
+        <h1 class="modal-header-title">Carrito.</h1>
+      `;
+    modalContainer.append(modalHeader);
   
+    const modalbutton = document.createElement("h1");
+    modalbutton.innerText = "x";
+    modalbutton.className = "modal-header-button";
   
-  <h1 class="modal-header-title">Carrito</h1>
-  <a class="btn btn-dark btn-size-auriculares btn-style" id="modalCierre">X</a>
+    modalbutton.addEventListener("click", () => {
+      modalContainer.style.display = "none";
+    });
   
-  `;
-
-  modalContainer.appendChild(modalHeader);
-
-  let modalCierre = document.getElementById ("modalCierre")
-  modalCierre.addEventListener("click", () => {
-    modalContainer.style.display = "none";
-
-  })
-
-
-  carrito.forEach((product) => {
-    let carritoContent = document.createElement("div");
+    modalHeader.append(modalbutton);
   
-    carritoContent.className = "modal-content";
-    carritoContent.innerHTML = `
-    
-    <img src="${product.img}">
-    <h3>${product.nombre}</h3>
-    <p>${product.precio}$</p>
-    
-    `;
-    
-    modalContainer.appendChild(carritoContent);
-
-  })
+    carrito.forEach((product) => {
+      let carritoContent = document.createElement("div");
+      carritoContent.className = "modal-content";
+      carritoContent.innerHTML = `
+          <img src="${product.img}">
+          <h3>${product.nombre}</h3>
+          <p>${product.precio} $</p>
+          <span class="restar"> - </span>
+          <!--recomiendo no escribir la palabra cantidad para que no quede tan largo :)-->
+          <p>${product.cant}</p>
+          <span class="sumar"> + </span>
+          <p>Total: ${product.cant * product.precio} $</p>
+          <span class="delete-product"> ❌ </span>
+        `;
   
+      modalContainer.append(carritoContent);
+      
+      let total = `${product.cant * product.precio}`
+      let restar = carritoContent.querySelector(".restar");
   
-  const total = carrito.reduce((acc, el) => acc + el.precio, 0);
-
-
-  const totalBuying = document.createElement("div");
-  totalBuying.className = "total-content";
-  totalBuying.innerHTML = `
-  Total a pagar ${total}$
-  <a class="btn btn-dark btn-size-auriculares btn-style" id="modalBtn">Comprar</a>
-  <a class="btn btn-dark btn-size-auriculares btn-style" id="modalCalculador">Calcular valor en Cuotas</a>
-  
-  `;
-
-  modalContainer.appendChild(totalBuying);
-
-let modalComprar = document.getElementById("modalBtn")
-modalComprar.addEventListener("click", () => {
-    for (let index = `${productos.stock}`; index > 0 ; index--) {
-        if (index > 0) {
-            alert("No hay mas stock");
+      restar.addEventListener("click", () => {
+        if (product.cant !== 1) {
+          product.cant--;
         }
-    }
-})
-
-
-  let modalCalculador = document.getElementById("modalCalculador")
+        saveLocal();
+        pintarCarrito();
+      });
   
-  modalCalculador.addEventListener("click", () => {
-    const calculadorCuotas = (a, b) => {
+      let sumar = carritoContent.querySelector(".sumar");
+      sumar.addEventListener("click", () => {
+        product.cant++;
+        saveLocal();
+        pintarCarrito();
+      });
+  
+      let eliminar = carritoContent.querySelector(".delete-product");
+      
+      eliminar.addEventListener("click", () => {
+        Swal.fire({
+          title: 'Estas seguro de eliminar el producto?',
+          showCancelButton: true,
+          confirmButtonText: 'Si',
+        }).then((result) => {
+          if (result.isConfirmed) {
+            Swal.fire({
+              icon: 'success'
+            })
+            eliminarProducto(product.id);
+          } 
+        })
+        
+      });
+  
+    });
+;
+
+  
+    const total = carrito.reduce((acc, el) => acc + el.precio * el.cant, 0);
+
+    const totalBuying = document.createElement("div");
+    totalBuying.innerHTML = `Total a pagar: ${total} $
+    <a class="btn btn-dark btn-size-auriculares btn-style" id="modalCalculador"> Calculador de cuotas </a>
+    <a class="btn btn-dark btn-size-auriculares btn-style" id="finalCompra"> Comprar </a>`;
+    
+    modalContainer.append(totalBuying);
+
+
+    const calculadora = document.getElementById("modalCalculador")
+    calculadora.addEventListener("click", () => {
+      const calculadorCuotas = (a, b) => {
         return parseFloat(a / b);
-    }
-
-    let cuotas = parseInt(prompt("Elija cantidad de cuotas"));
-
+      }
+    (async () =>  {const {value: cuotas} = await Swal.fire({
+      text: `Seleccione cantidad de cuotas`,
+      input: `select`,
+      position: `center`,
+      icon: `question`,
+      inputPlaceholder: `Cuotas`,
+      inputOptions: {
+        3: 3,
+        6: 6,
+        12: 12,
+      }
+    })
+   
     let resultado = calculadorCuotas (`${total}`, `${cuotas}`);
-    switch(cuotas){
-        case 3:
-            if(cuotas = 3){
-                alert (`Su resultado quedaria en ${resultado}$`);
-            }
-            break;
-        case 6:
-            if(cuotas = 6){
-                alert (`Su resultado quedaria en ${resultado}$`);
-            }
-            break;
-        case 12:
-            if(cuotas = 12){
-                alert (`Su resultado quedaria en ${resultado}$`);
-            }
-            break;
-        default:
-            while (cuotas !== 3 && 6 && 12) {
-                alert("Elija un número entre 3, 6 y 12 respectivo a las cuotas");
-                cuotas = parseInt(prompt("Elija cuantas cuotas"));
-            }
-            break
-       }
+    Swal.fire({
+      text: `Te quedaría un total de ${resultado} en ${cuotas} cuotas`
+    })
+    })()
+    
+    finalCompra.addEventListener("click", () => {
+    let contCompra = document.createElement("div")
+    contCompra.className = "modal-content-form";
+    contCompra.innerHTML = `
+    <form action="contacto.html" enctype="application/x-www-form-urlencoded" method="post" class="col-lg-5 col-sm-12 col-xs-12 flex-form mx-auto">
+    <div class="hover-form col-sm-12 col-xs-12 rounded">
+    <h1>  Registrese </h1>
+      <div class="mb-3 label-w">
+        <label for="exampleFormControlInput1" class="form-label">Email</label>
+        <input class="form-control" type="email" aria-label="default input example" placeholder="nombre@email.com">
+      </div>
+
+      <div class="mb-3 label-w">
+        <label for="exampleFormControlInput1" class="form-label">Contraseña</label>
+        <input type="password" class="form-control " id="exampleFormControlInput1">
+      </div>
+
+      <div class="check-btn-flex pb-3">
+        <div class="form-check mx-auto">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault">Acepta los términos y condiciones</label>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-dark btn-style btn-form-mobile" id="realizarCompra">Enviar</button>
+    </div>
+  </form>
+    `
+    
+    modalContainer.append(contCompra)
+    let realizarCompra = document.getElementById("realizarCompra")
+    realizarCompra.addEventListener("click", () => {
+      Swal.fire({
+        title: 'Se ha registrado exitosamente!',
+        icon: 'success'
+      })
+    })
+    })
   })
-})
+  };
+  
+
+  verCarrito.addEventListener("click", () => {
+    pintarCarrito()
+  })
+
+
+  const eliminarProducto = (id) => {
+    const foundId = carrito.find((element) => element.id === id);
+  
+    console.log(foundId);
+  
+    carrito = carrito.filter((carritoId) => {
+      return carritoId !== foundId;
+    });
+
+    carritoCounter();
+    saveLocal();
+    pintarCarrito();
+    
+  };
+
+  
+
+  
+ 
+  carritoCounter();
